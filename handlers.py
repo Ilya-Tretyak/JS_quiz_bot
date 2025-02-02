d96fc7b0f373274cb7ab1266bec3bf5874cf51b1
ILYA TRETYAK <105990567+Ilya-Tretyak@users.noreply.github.com>
2025-02-02 18:38:18 +0300

    Реализация викторины

--- handlers.py
+++ handlers.py
@@ -2,41 +2,60 @@ from aiogram import types, Router, F
 from aiogram.filters.command import Command
 
 import kb
-from state import StatusTest
 from questions import questions
 
 router = Router()
 
 
+class QuizState:
+    def __init__(self):
+        self.current_question = 0
+        self.correct_answer = 0
+
+
+quiz_state = QuizState()
+
+
 @router.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("🎯 Выбери уровень сложности:", reply_markup=kb.chooce_level)
-    
 
-@router.message(F.text=="👶Новичок")
+
+@router.message(F.text == "👶Новичок")
 async def send_quiz(message: types.Message):
-    current_question = 0
-    correct_answer = 0
+    quiz_state.correct_answer = 0
+    quiz_state.current_question = 0
 
-    while True:
-        question = questions[current_question]
+    await send_next_question(message)
 
-        kb = []
-        for option in question['answer_options']:
-            kb.append([types.KeyboardButton(text=option)])
-        
-        keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
-        await message.answer(question['text'], reply_markup=keyboard)
 
-        user_answer = None
-        while user_answer not in question['answer_options']:
-            user_answer = await message.answer("Пожалуйста, выберите один из вариантов ответа и напишите его.")
-        
-        if user_answer == question['options'][question['right_answer']]:
-            correct_answer += 1
-        
-        current_question += 1
-        if current_question >= len(questions):
-            await message.answer(f"Тест завершен!\nВы правильно ответили на {correct_answer} вопросов.")
-            break
-        
+async def send_next_question(message: types.Message):
+    question = questions[quiz_state.current_question]
+    keyboard_buttons = [[types.InlineKeyboardButton(text=option, callback_data=option)]
+                        for option in question['answer_options']]
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=keyboard_buttons)
+
+    await message.answer(question['text'], reply_markup=keyboard)
+
+
+@router.callback_query()
+async def handler_answer(callback_query: types.CallbackQuery):
+    question = questions[quiz_state.current_question]
+
+    print(callback_query.data)
+    print(question['answer_options'])
+
+    if callback_query.data not in question['answer_options']:
+        await callback_query.answer("Пожалуйста, выберите один из вариантов ответа!")
+        return
+
+    if callback_query.data == question['right_answer']:
+        quiz_state.correct_answer += 1
+
+    quiz_state.current_question += 1
+    if quiz_state.current_question >= len(questions):
+        await callback_query.message.answer(f"Тест завершен!\nВы правильно ответили на {quiz_state.correct_answer} вопросов.")
+    else:
+        await send_next_question(callback_query.message)
+
+    await callback_query.answer()
