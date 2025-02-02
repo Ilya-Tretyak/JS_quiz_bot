8142bc682f3d5037a534ad199a3399959833fc23
ILYA TRETYAK <105990567+Ilya-Tretyak@users.noreply.github.com>
2025-02-02 18:53:48 +0300

    Добавление функции удаления сообщений

--- handlers.py
+++ handlers.py
@@ -1,6 +1,5 @@
 from aiogram import types, Router, F
 from aiogram.filters.command import Command
-
 import kb
 from questions import questions
 
@@ -11,6 +10,7 @@ class QuizState:
     def __init__(self):
         self.current_question = 0
         self.correct_answer = 0
+        self.message_id = None  # Добавляем хранение id сообщения с вопросом
 
 
 quiz_state = QuizState()
@@ -18,35 +18,42 @@ quiz_state = QuizState()
 
 @router.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("🎯 Выбери уровень сложности:", reply_markup=kb.chooce_level)
+    await message.answer("🎯 Выбери уровень сложности:", reply_markup=kb.choose_level)
 
 
 @router.message(F.text == "👶Новичок")
 async def send_quiz(message: types.Message):
-    quiz_state.correct_answer = 0
     quiz_state.current_question = 0
+    quiz_state.correct_answer = 0
+    quiz_state.message_id = None  # Сбрасываем id сообщения
 
     await send_next_question(message)
 
 
 async def send_next_question(message: types.Message):
     question = questions[quiz_state.current_question]
-    keyboard_buttons = [[types.InlineKeyboardButton(text=option, callback_data=option)]
+    keyboard_buttons = [types.InlineKeyboardButton(text=option, callback_data=option)
                         for option in question['answer_options']]
-    keyboard = types.InlineKeyboardMarkup(inline_keyboard=keyboard_buttons)
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=[keyboard_buttons])
 
-    await message.answer(question['text'], reply_markup=keyboard)
+    if quiz_state.message_id:
+        await message.bot.edit_message_text(
+            chat_id=message.chat.id,
+            message_id=quiz_state.message_id,
+            text=question['text'],
+            reply_markup=keyboard
+        )
+    else:
+        sent_message = await message.answer(question['text'], reply_markup=keyboard)
+        quiz_state.message_id = sent_message.message_id  # Сохраняем id отправленного сообщения
 
 
 @router.callback_query()
-async def handler_answer(callback_query: types.CallbackQuery):
+async def handle_answer(callback_query: types.CallbackQuery):
     question = questions[quiz_state.current_question]
 
-    print(callback_query.data)
-    print(question['answer_options'])
-
     if callback_query.data not in question['answer_options']:
-        await callback_query.answer("Пожалуйста, выберите один из вариантов ответа!")
+        await callback_query.answer("Пожалуйста, выберите один из предложенных вариантов ответа.")
         return
 
     if callback_query.data == question['right_answer']:
@@ -54,7 +61,14 @@ async def handler_answer(callback_query: types.CallbackQuery):
 
     quiz_state.current_question += 1
     if quiz_state.current_question >= len(questions):
-        await callback_query.message.answer(f"Тест завершен!\nВы правильно ответили на {quiz_state.correct_answer} вопросов.")
+        await callback_query.message.answer(
+            f"Тест завершен!\nВы правильно ответили на {quiz_state.correct_answer} вопросов."
+        )
+        if quiz_state.message_id:
+            await callback_query.message.bot.delete_message(
+                    chat_id=callback_query.message.chat.id,
+                    message_id=quiz_state.message_id
+                )
     else:
         await send_next_question(callback_query.message)
 
