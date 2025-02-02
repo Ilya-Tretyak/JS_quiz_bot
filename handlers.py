c17be5159630369b22697088956b9fc011e91cb2
ILYA TRETYAK <105990567+Ilya-Tretyak@users.noreply.github.com>
2025-02-02 20:20:09 +0300

    Доработка: -Изменение вопросов по теме JS -одновременно несколько пользователей

--- handlers.py
+++ handlers.py
@@ -1,7 +1,7 @@
 from aiogram import types, Router, F
 from aiogram.filters.command import Command
 import kb
-from questions import questions
+from questions import questions_for_beginner
 
 router = Router()
 
@@ -13,25 +13,35 @@ class QuizState:
         self.message_id = None  # Добавляем хранение id сообщения с вопросом
 
 
-quiz_state = QuizState()
+user_states = {}
 
 
 @router.message(Command("start"))
 async def cmd_start(message: types.Message):
+    user_id = message.from_user.id
+    user_states[user_id] = QuizState()
     await message.answer("🎯 Выбери уровень сложности:", reply_markup=kb.choose_level)
 
 
 @router.message(F.text == "👶Новичок")
 async def send_quiz(message: types.Message):
-    quiz_state.current_question = 0
-    quiz_state.correct_answer = 0
-    quiz_state.message_id = None  # Сбрасываем id сообщения
+    user_id = message.from_user.id
+    quiz_state = user_states.get(user_id)
 
-    await send_next_question(message)
+    if quiz_state:
+        quiz_state.current_question = 0
+        quiz_state.correct_answer = 0
+        quiz_state.message_id = None  # Сбрасываем id сообщения
+
+    await send_next_question(message, user_id)
 
 
-async def send_next_question(message: types.Message):
-    question = questions[quiz_state.current_question]
+async def send_next_question(message: types.Message, user_id: int):
+    quiz_state = user_states.get(user_id)
+    if not quiz_state:
+        return
+
+    question = questions_for_beginner[quiz_state.current_question]
     keyboard_buttons = [types.InlineKeyboardButton(text=option, callback_data=option)
                         for option in question['answer_options']]
     keyboard = types.InlineKeyboardMarkup(inline_keyboard=[keyboard_buttons])
@@ -50,7 +60,12 @@ async def send_next_question(message: types.Message):
 
 @router.callback_query()
 async def handle_answer(callback_query: types.CallbackQuery):
-    question = questions[quiz_state.current_question]
+    user_id = callback_query.from_user.id
+    quiz_state = user_states.get(user_id)
+    if not quiz_state:
+        return
+
+    question = questions_for_beginner[quiz_state.current_question]
 
     if callback_query.data not in question['answer_options']:
         await callback_query.answer("Пожалуйста, выберите один из предложенных вариантов ответа.")
@@ -60,16 +75,18 @@ async def handle_answer(callback_query: types.CallbackQuery):
         quiz_state.correct_answer += 1
 
     quiz_state.current_question += 1
-    if quiz_state.current_question >= len(questions):
+    if quiz_state.current_question >= len(questions_for_beginner):
         await callback_query.message.answer(
             f"Тест завершен!\nВы правильно ответили на {quiz_state.correct_answer} вопросов."
         )
+        user_states.pop(user_id, None)
+
         if quiz_state.message_id:
             await callback_query.message.bot.delete_message(
                     chat_id=callback_query.message.chat.id,
                     message_id=quiz_state.message_id
                 )
     else:
-        await send_next_question(callback_query.message)
+        await send_next_question(callback_query.message, user_id)
 
     await callback_query.answer()
--- questions.py
+++ questions.py
@@ -1,66 +1,111 @@
-questions = [
+questions_for_beginner = [
     {
-        'text': 'Сколько спутников у Марса?',
-        'answer_options': ['9', '2', '5', '1'],
+        'text': 'Что выведет console.log(typeof NaN)?',
+        'answer_options': ['number', 'string', 'undefined', 'NaN'],
+        'right_answer': 'number'
+    },
+
+    {
+        'text': 'Как объявить переменную с блочной областью видимости?',
+        'answer_options': ['var', 'let', 'const', 'function'],
+        'right_answer': 'let'
+    },
+
+    {
+        'text': 'Что вернет Boolean('')?',
+        'answer_options': ['true', 'false', 'null', 'undefined'],
+        'right_answer': 'false'
+    },
+
+    {
+        'text': 'Что такое undefined?',
+        'answer_options': ['Тип данных', 'Значения', 'Оператор', 'Функция'],
+        'right_answer': 'Значения'
+    },
+
+    {
+        'text': 'Как добавить элемент в конец массива?',
+        'answer_options': ['push()', 'pop()', 'shift()', 'unshift()'],
+        'right_answer': 'push()'
+    },
+
+    {
+        'text': 'Что вернет parseInt("12px")?',
+        'answer_options': ['12', 'NaN', '12px', 'Ошибка'],
+        'right_answer': '12'
+    },
+
+    {
+        'text': 'Как проверить массив?',
+        'answer_options': ['typeof arr', 'arr.isArray()', 'Array.isArray(arr)', 'arr.typeOf()'],
+        'right_answer': 'Array.isArray(arr)'
+    },
+
+    {
+        'text': 'Что делает оператор ===?',
+        'answer_options': ['Приведение типов', 'Проверка без приведения', 'Присваивание', 'Сравнение объектов'],
+        'right_answer': 'Проверка без приведения'
+    },
+
+    {
+        'text': 'Что выведет console.log(1 + "1")?',
+        'answer_options': ['2', '11', 'NaN', 'Ошибка'],
+        'right_answer': '11'
+    },
+
+    {
+        'text': 'Как создать объект?',
+        'answer_options': ['{}', 'Object.create()', 'new Object', 'Все варианты'],
+        'right_answer': 'Все варианты'
+    },
+    {
+        'text': 'Что делает Array.prototype.slice()?',
+        'answer_options': ['Мутирует массив', 'Возвращает копию части массива', 'Удаляет  элементы', 'Сортирует массив'],
+        'right_answer': 'Возвращает копию части массива'
+    },
+    {
+        'text': 'Как остановить цикл for?',
+        'answer_options': ['break', 'return', 'exit', 'continue'],
+        'right_answer': 'break'
+    },
+    {
+        'text': 'Что такое null?',
+        'answer_options': ['Объект', 'Примитив', 'Функция', 'Операнд'],
+        'right_answer': 'Примитив'
+    },
+    {
+        'text': 'Как работает switch?',
+        'answer_options': ['Строгое сравнение', 'Приведение типов', 'Только числа', 'Через оператор in'],
+        'right_answer': 'Строгое сравнение'
+    },
+    {
+        'text': 'Что делает Math.floor(3.9)?',
+        'answer_options': ['4', '3', '3.0', 'NaN'],
+        'right_answer': '3'
+    },
+    {
+        'text': 'Как вызвать функцию?',
+        'answer_options': ['call()', 'apply()', '()', 'Все варианты'],
+        'right_answer': 'Все варианты'
+    },
+    {
+        'text': 'Что такое IIFE?',
+        'answer_options': ['Функция-конструктор', 'Самовызывающаяся функция', 'Асинхронная функция', 'Стрелочная функция'],
+        'right_answer': 'Самовызывающаяся функция'
+    },
+    {
+        'text': 'Что выведет console.log(typeof [])?',
+        'answer_options': ['array', 'object', 'undefined', 'null'],
+        'right_answer': 'object'
+    },
+    {
+        'text': 'Как работает String.prototype.slice(1, 4) для "abcdef"?',
+        'answer_options': ['abc', 'bcd', 'abcd', 'bcde'],
+        'right_answer': 'bcd'
+    },
+    {
+        'text': 'Что вернет "Hello".indexOf("l")?',
+        'answer_options': ['2', '3', '1', '-1'],
         'right_answer': '2'
-    },
-
-    {
-        'text': 'Как мы ещё называем хлорид натрия?',
-        'answer_options': ['Соль', 'Сахар', 'Хлор', 'Отбеливатель'],
-        'right_answer': 'Соль'
-    },
-
-    {
-        'text': 'За сколько дней Луна совершает полный оборот вокруг Земли?',
-        'answer_options': ['7', '27', '30', '365'],
-        'right_answer': '27'
-    },
-
-    {
-        'text': 'У какого млекопитающего нет голосовых связок?',
-        'answer_options': ['Бегемот', 'Пантера', 'Жираф', 'Крот'],
-        'right_answer': 'Жираф'
-    },
-
-    {
-        'text': 'В какой стране образовалась легендарная рок-группа AC/DC?',
-        'answer_options': ['США', 'Великобритания', 'Канада', 'Австралия'],
-        'right_answer': 'Австралия'
-    },
-
-    {
-        'text': 'В какой стране самое большое количество озер?',
-        'answer_options': ['Россия', 'США', 'Канада', 'Китай'],
-        'right_answer': 'Канада'
-    },
-
-    {
-        'text': 'Кто придумал Шерлока Холмса?',
-        'answer_options': ['Агата Кристи', 'Артур Конан Дойл', 'Эдгар Аллан По', 'Стиг Ларссон'],
-        'right_answer': 'Артур Конан Дойл'
-    },
-
-    {
-        'text': 'В какой стране изобрели мороженое?',
-        'answer_options': ['Индия', 'Канада', 'Китай', 'Япония'],
-        'right_answer': 'Китай'
-    },
-
-    {
-        'text': 'Первая женщина, получившая Нобелевскую премию (в 1903 году)?',
-        'answer_options': ['Мария Кюри', 'Фрэнсис Арнольд', 'Розалинд Франклин', 'Дороти Ходжкин'],
-        'right_answer': 'Мария Кюри'
-    },
-
-    {
-        'text': 'Что сжигают в романе Рэя Брэдбери "451 градус по Фаренгейту"?',
-        'answer_options': ['Книги', 'Одежду', 'Дома', 'Деньги'],
-        'right_answer': 'Книги'
-    },
-
+    }
 ]
-
-print(questions[0]['right_answer'])
-
-
